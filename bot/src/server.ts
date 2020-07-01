b269cd74486e45c0e906d7fbeb04105847bb87a5
Matheus de Sousa <matheussousaf7@gmail.com>
2020-07-01 08:32:18 -0300

    :bug: corrigindo credentials

--- bot/src/server.ts
+++ bot/src/server.ts
@@ -2,18 +2,19 @@ import express, { Request, Response } from "express";
 import * as bodyParser from "body-parser";
 import cors from "cors";
 import MessagingController from "./services/messaging";
+
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (request: Request, response: Response) => {
-  return response.send({ version: 1.0 });
-});
+app.get("/", (request: Request, response: Response) =>
+  response.send({ version: 1.0 })
+);
 
 app.post("/", MessagingController.receiveMessage);
 
